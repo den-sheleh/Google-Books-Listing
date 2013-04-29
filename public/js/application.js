1225913a3e46275c70f08da02a32ee0f915b24d8
Dennis Sheleh <den.sheleh@gmail.com>
2013-04-29 22:28:43 +0300

    Fancybox gallery for Books

---
+++ public/js/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".fancybox").fancybox({
+    helpers:  {
+      overlay : {
+          css : {
+              'background' : 'rgba(255,255,255,0.5)'
+          }
+      }
+    }
+  });
+})
