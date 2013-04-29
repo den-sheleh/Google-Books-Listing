f750579b18a60ab8f7bcc6783425cf7ef941b75c
Dennis Sheleh <den.sheleh@gmail.com>
2013-04-29 21:34:55 +0300

    BooksCollection: Cache Method which return name of cache key for specific key

--- lib/models/books_collection.rb
+++ lib/models/books_collection.rb
@@ -1,4 +1,5 @@
 class BooksCollection
+  CACHE_KEY = 'google_books_response_page_'
   QUERY_TERM = 'Ruby on Rails'
   RESULTS_PER_PAGE = 6
   PROXY_IP = '78.47.116.164'
@@ -6,6 +7,10 @@ class BooksCollection
   class << self
     def get_page(page = 1)
       @collection = GoogleBooks.search(QUERY_TERM, { count: RESULTS_PER_PAGE, page: page.to_i }, PROXY_IP)
+    end
+
+    def cache_key(page)
+      CACHE_KEY + page.to_s
     end
   end
 end
