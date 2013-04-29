8f201ad4eea33a9a03b8799742c42e2d8befe2d2
Dennis Sheleh <den.sheleh@gmail.com>
2013-04-29 21:23:48 +0300

    Init commit: application skeleton

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+
+gem 'thin'
+gem 'haml'
+gem 'sinatra-dalli'
+gem 'googlebooks'
+
+gem 'shotgun'
+gem 'pry'
---
+++ app.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH << '.'
+
+%w(rubygems bundler/setup sinatra sinatra/dalli
+  googlebooks will_paginate will_paginate-bootstrap pry).each { |lib| require lib }
+
+Dir[File.join('lib', '**', '*.rb')].each { |file| require file }
+
+get '/:page?' do |page|
+  @page = page || 1
+
+  cache BooksCollection.cache_key(@page) do
+    @books = BooksCollection.get_page(@page)
+    haml :index
+  end
+end
